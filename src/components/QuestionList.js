131e1fe4be75bc37e22ce1bfc16a7b61b1df274b
emily <emilymomata@gmail.com>
2024-02-17 13:44:59 -0500

    Completed successfuly

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,71 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-function QuestionList() {
+function QuestionList(onAdd) {
+    //Load list items when component loads
+  const API = "http://localhost:4000/questions"
+  const [allQuestions, setAllQuestions] = useState([])
+
+  useEffect(() => {
+    fetchQuestions()
+  },[onAdd])
+
+  async function fetchQuestions(){
+    const questions = await fetch(API)
+    const results = await questions.json()
+    // console.log(results)
+    setAllQuestions(results)
+  }
+
+  async function updateAnswers(url,method,object=null){
+    const updateObj = {
+      method : method,
+      headers : { "Content-Type": "application/json" },
+      body : object
+    }
+    await fetch(url,updateObj)
+  }
+
+  const handleChange = (e) => {
+    const url = `${API}/${e.target.value}`
+    const object = JSON.stringify({
+      correctIndex : e.target.selectedIndex
+    })
+    updateAnswers(url,"PATCH",object)
+  }
+
+  const handleDelete = async (id) => {
+    //Change component state
+    const questions = allQuestions.filter(question => {
+      return question.id !== id
+    })
+    setAllQuestions(questions)
+
+    const url = `${API}/${id}`
+    // console.log(id)
+    await updateAnswers(url,"DELETE")
+  }
+
+  // console.log(allQuestions)
+  const displayQuestions = allQuestions.map((question) => {
+    return (
+      <li key={question.id}>
+        <form>
+          <label>{question.prompt}</label>
+          <select name="answers" id="answers"  onChange={handleChange}>
+            {question.answers.map((answer) => {
+              return <option key={answer} value={question.id}>{answer}</option>
+            })}
+          </select>
+        </form>
+        <button onClick={() => handleDelete(question.id)}>DELETE</button>
+      </li>
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayQuestions}</ul>
     </section>
   );
 }
